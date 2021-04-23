00e2a1897cb7e1d53933c8b34b84d6074f3b1676
Haley Parish <heparish@ucdavis.edu>
2021-04-22 19:47:47 -0700

    added date and clock

---
+++ script.js
@@ -0,0 +1,43 @@
+//current day
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); 
+var yyyy = today.getFullYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
+
+document.getElementById("currentDay").textContent = today;
+
+
+//clock function from classwork
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("currentTime").innerText = time;
+    document.getElementById("currentTime").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
+
